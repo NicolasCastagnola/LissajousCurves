cfd302682d8499f39ca47437cf4d7ffb4086b8d8
Nicolas Castagnola <castagnola.nicolas@gmail.com>
2023-05-03 00:41:14 -0300

    General updates + add trail stuff

--- Assets/Scripts/LissajousCurve.cs
+++ Assets/Scripts/LissajousCurve.cs
@@ -1,27 +1,57 @@
+using System;
 using UnityEngine;
+
+public enum TrailPreset { White, Green, Red, Blue, Rainbow }
+
 public class LissajousCurve : MonoBehaviour
 {
     private float _currentTime;
     private Vector3 finalPosition;
     
+    [Header("TRAIL VALUES")]
+    public float speedMultiplier;
+    public float trailDuration;
+    
     [Header("EQUATION VALUES")]
-    public float X = 4f;
-    public float Y = 2f;
-    public float α = 4f;
-    public float β = 6f;
-    public float delta = 2f;
+    public float X;
+    public float Y;
+    public float α;
+    public float β;
+    public float delta;
     public TrailRenderer TrailRenderer { get; private set; }
     public void Start() => TrailRenderer = GetComponent<TrailRenderer>();
     public void Update() => CalculateStandardLissajousCurve();
     private void CalculateStandardLissajousCurve()
     {
-        _currentTime = Time.time;
+        _currentTime = Time.time * speedMultiplier;
         
         finalPosition = transform.position;
-        
+
         finalPosition.x = X * Mathf.Sin(α * _currentTime + delta);
         finalPosition.y = Y * Mathf.Sin(β * _currentTime);
 
         transform.position = finalPosition;
+    }
+    public void SetTrailColor(TrailPreset trailPreset)
+    {
+        switch (trailPreset)
+        {
+            case TrailPreset.White:
+                break;
+            
+            case TrailPreset.Green:
+                break;
+            
+            case TrailPreset.Red:
+                break;
+            
+            case TrailPreset.Blue:
+                break;
+            
+            case TrailPreset.Rainbow:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(trailPreset), trailPreset, null);
+        }
     }
 }
--- Assets/Scripts/UI_CurveController.cs
+++ Assets/Scripts/UI_CurveController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,51 +7,79 @@ using System.Globalization;
 
 public class UI_CurveController : MonoBehaviour
 {
+    [SerializeField] private List<Gradient> _colorGradients;
+    [SerializeField] private TMP_Dropdown trailDropdown;
     [SerializeField] private Toggle clearTrailToggle;
     [SerializeField] private TMP_Text version;
 
     [SerializeField] private LissajousCurve _lissajousCurve;
-    [SerializeField, Header("Sliders")] private Slider XSlider, YSlider, αSlider, βSlider, deltaSlider;
-    [SerializeField, Header("Current Values")] private TMP_Text XCurrentSliderValue, YCurrentSliderValue, αCurrentSliderValue, βCurrentSliderValue, deltaCurrentSliderValue;
+    [SerializeField, Header("Sliders")] private Slider XSlider, YSlider, αSlider, βSlider, deltaSlider, speedSlider, trailTime;
+    [SerializeField, Header("Current Values")] private TMP_Text XCurrentSliderValue, YCurrentSliderValue, αCurrentSliderValue, βCurrentSliderValue, deltaCurrentSliderValue,speedCurrentSliderValue, trailRefreshCurrentSliderValue ;
 
     private void OnEnable()
     {
         version.text = $" VER. {Application.version}";
         
-        XSlider.onValueChanged.AddListener(x => {
-            XCurrentSliderValue.text = x.ToString("0.0",CultureInfo.InvariantCulture);
+        InitializeDropdown();
+        
+        trailTime.onValueChanged.AddListener(x => 
+        {
+            trailRefreshCurrentSliderValue.text = x.ToString("0.0", CultureInfo.InvariantCulture);
+            _lissajousCurve.TrailRenderer.time = x;
+            
+            if (clearTrailToggle.isOn) _lissajousCurve.TrailRenderer.Clear();
+        });
+        speedSlider.onValueChanged.AddListener(x => 
+        {
+            speedCurrentSliderValue.text = x.ToString("0.0", CultureInfo.InvariantCulture);
+            _lissajousCurve.speedMultiplier = x;
+            
+            if (clearTrailToggle.isOn) _lissajousCurve.TrailRenderer.Clear();
+        });
+        XSlider.onValueChanged.AddListener(x => 
+        {
+            XCurrentSliderValue.text = x.ToString("0.0", CultureInfo.InvariantCulture);
             _lissajousCurve.X = x;
             
             if (clearTrailToggle.isOn) _lissajousCurve.TrailRenderer.Clear();
         });
-
-        YSlider.onValueChanged.AddListener(x => {
-            YCurrentSliderValue.text = x.ToString("0.0",CultureInfo.InvariantCulture);
+        YSlider.onValueChanged.AddListener(x =>
+        {
+            YCurrentSliderValue.text = x.ToString("0.0", CultureInfo.InvariantCulture);
             _lissajousCurve.Y = x;
             
             if (clearTrailToggle.isOn) _lissajousCurve.TrailRenderer.Clear();
         });
-
-        αSlider.onValueChanged.AddListener(x => {
-            αCurrentSliderValue.text = x.ToString("0.0",CultureInfo.InvariantCulture);
+        αSlider.onValueChanged.AddListener(x =>
+        {
+            αCurrentSliderValue.text = x.ToString("0.0", CultureInfo.InvariantCulture);
             _lissajousCurve.α = x;
             
             if (clearTrailToggle.isOn) _lissajousCurve.TrailRenderer.Clear();
         });
-
-        βSlider.onValueChanged.AddListener(x => {
-            βCurrentSliderValue.text = x.ToString("0.0",CultureInfo.InvariantCulture);
+        βSlider.onValueChanged.AddListener(x => 
+        {
+            βCurrentSliderValue.text = x.ToString("0.0", CultureInfo.InvariantCulture);
             _lissajousCurve.β = x;
             
             if (clearTrailToggle.isOn) _lissajousCurve.TrailRenderer.Clear();
 
         });
-
-        deltaSlider.onValueChanged.AddListener(x => {
-            deltaCurrentSliderValue.text = x.ToString("0.0",CultureInfo.InvariantCulture);
+        deltaSlider.onValueChanged.AddListener(x => 
+        {
+            deltaCurrentSliderValue.text = x.ToString("0.0", CultureInfo.InvariantCulture);
             _lissajousCurve.delta = x;
             
             if (clearTrailToggle.isOn) _lissajousCurve.TrailRenderer.Clear();
         });
+
+    }
+ 
+    private void InitializeDropdown()
+    {
+        trailDropdown.ClearOptions();
+        string[] enumNames = Enum.GetNames(typeof(TrailPreset));
+        trailDropdown.AddOptions(new List<string>(enumNames));
+        trailDropdown.onValueChanged.AddListener(x => _lissajousCurve.TrailRenderer.colorGradient = _colorGradients[x]);
     }
 }
